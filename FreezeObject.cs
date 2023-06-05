e13a8fbc886b5b8ef9133d7f5a4f236c16e98758
Robert Rumney <robertrumney@gmail.com>
2023-06-05 13:28:51 +0200

    Update FreezeObject.cs

--- FreezeObject.cs
+++ FreezeObject.cs
@@ -1,29 +1,47 @@
 using UnityEngine;
 using System.Collections;
 
-public class FreezeObject : MonoBehaviour 
+public class FreezeObject : MonoBehaviour
 {
     public float timeToFreeze = 2.0f;
-    public float freezeAmount = 0.5f;
+    public float freezeAmount = 0.7f;
     private Renderer objectRenderer;
 
-    private void Awake() 
+    private void Awake()
     {
         objectRenderer = GetComponent<Renderer>();
+        AssignShader();
         Freeze();
     }
 
-    public void Freeze() 
+    public void AssignShader()
+    {
+        // Load the shader
+        Shader iceShader = Shader.Find("Custom/IceShader");
+
+        // Assign the shader to the material
+        if (iceShader != null)
+        {
+            objectRenderer.material.shader = iceShader;
+        }
+        else
+        {
+            Debug.LogError("Ice shader not found. Please ensure it's included in the build.");
+        }
+    }
+
+
+    public void Freeze()
     {
         StartCoroutine(FreezeOverTime(timeToFreeze));
     }
 
-    private IEnumerator FreezeOverTime(float time) 
+    private IEnumerator FreezeOverTime(float time)
     {
         float elapsed = 0;
         float currentFreezeAmount = objectRenderer.material.GetFloat("_FrozenAmount");
 
-        while (elapsed < time) 
+        while (elapsed < time)
         {
             elapsed += Time.deltaTime;
             float newValue = Mathf.Lerp(currentFreezeAmount, freezeAmount, elapsed / time);
